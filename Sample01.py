4097265dc97d64f43d3642bca46432d5ab77061e
burlatabraham2004-svg <burlat.abraham2004@gmail.com>
2026-01-22 14:32:26 +0800

    Add student information display in Sample01.py

---
+++ Sample01.py
@@ -0,0 +1,11 @@
+student_ID = 2025300371 
+student_name = "Abraham L. Burlat"
+course = "BS in Information Technology"
+year_level = "1st Year College"
+
+print("STUDENT INFORMATION")
+print("--------------------")
+print("Student ID: ", student_ID)
+print("Student Name: ", student_name)
+print("Course: ", course)
+print("Year Level: ", year_level)
